e10ac822d46c6b5896fa131f915cb29ad331818f
Seojin Kim <1106laura@naver.com>
2023-06-01 19:54:44 +0900

    chore: add data to 100

--- src/resources/images/webFonts/sync.sh
+++ src/resources/images/webFonts/sync.sh
@@ -1,5 +1,5 @@
 # iterate over all directories
 for dir in */ ; do
     # copy the contents of each directory to the corresponding directory in f2
-    rsync -av --ignore-existing "$dir"/ /Users/kimseojin/Desktop/이번학기/지능형비전/more/5_renamed/"$dir"/
+    rsync -av --ignore-existing "$dir"/ /Users/kimseojin/Desktop/이번학기/지능형비전/more_0531/5_renamed/"$dir"/
 done
